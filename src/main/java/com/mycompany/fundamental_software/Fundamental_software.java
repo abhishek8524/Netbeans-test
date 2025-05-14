9cde1573a6b63a723379ac19c90f99c8bc91f162
abhishek8524 <abhish@sheridancollege.ca>
2025-05-14 17:56:10 -0400

    student master copy of code

---
+++ src/main/java/com/mycompany/fundamental_software/Fundamental_software.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fundamental_software;
+
+/**
+ *
+ * @author abhis
+ */
+public class Fundamental_software {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+//        Student s1= new Student();
+
+        
+    }
+}
